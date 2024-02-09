e66908d0db35b2e47cf872277704060dc8fede95
Angel-Raymundo <jimray214@gmail.com>
2024-02-08 20:59:18 -0600

    changes

--- app/yay/page.js
+++ app/yay/page.js
@@ -1,10 +1,9 @@
 export default function page() {
-
   return (
-    <div className='box'>
-      <img alt='bugcat yayyy' className='image' src='/bugcat-yay.gif' />
-      Yayyy~
+    <div className="box">
+      Sospechaba que dirias que si ;)
+      <img alt="bugcat yayyy" className="image" src="/bugcat-yay.gif" />
+      Te amo mucho gabisita
     </div>
-  )
-
+  );
 }
